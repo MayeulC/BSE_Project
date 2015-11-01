0beb6873fed2d62e7e2405db4501807a3823e20b
Mayeul Cantan <mayeul.cantan@gmail.com>
2015-11-01 17:18:49 +0100

    Added The files which will contain package-related informations

---
+++ LIB_BSE_Packages.h
@@ -0,0 +1,32 @@
+/*
+   This program is designed to be compiled with Keil µVision4's ANSI C
+   compiler, and ran on a 8051F020 microcontroller.
+
+   This file contains the function prototypes for the package management
+   system.
+
+   Copyright (C) 2015  Aydin Alperen <alperen.aydin@cpe.fr>
+   Copyright (C) 2015  Cantan Mayeul <mayeul.cantan@cpe.fr>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#ifndef LIB_BSE_PACKAGES_H
+#define LIB_BSE_PACKAGES_H
+#include "defines.h"
+
+
+
+#endif //LIB_BSE_PACKAGES_H

ebed8651ff6571842844cb103eed5acf0827fc89
Mayeul Cantan <mayeul.cantan@gmail.com>
2016-01-29 21:46:41 +0100

    Added a License header to Declarations_GPIO_BSE.h

--- Declarations_GPIO_BSE.h
+++ Declarations_GPIO_BSE.h
@@ -1,3 +1,28 @@
+/*
+   This program is designed to be compiled with Keil µVision4's ANSI C
+   compiler, and ran on a 8051F020 microcontroller.
+
+   This file contains the GPIO declarations, that assign GPIO pins to
+   the different functions performed. This is one of the starting files
+   that were given at the begining of the project.
+
+   Copyright (C) 2015  François Joly <francois.joly@cpe.fr>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
 //------------------------------------------------------------------------------------
 // Déclarations des GPIOs Projet BSE 2013
 //------------------------------------------------------------------------------------
